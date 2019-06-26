c51058b1a2a7df75e8634f758cfa599e65d962a4
Oluwatomisin Lalude <oluwatomisin1605@gmail.com>
2019-06-26 16:45:04 +0100

    Configure node server

---
+++ index.js
@@ -0,0 +1,40 @@
+let http = require('http');
+let fs = require('fs');
+let path = require('path');
+
+const ip = '127.0.0.1';
+const port = 3000;
+
+http.createServer = ((request, response) => {
+  let file = '.' + request.url; //Add . to URL to convert it to a local file path
+  if (file== './') file = './index.html'; //Redirect / to serve index.html
+  let extension = String(path.extname(file)).toLowerCase(); //Extract requested file's extension
+  let mime = { '.html': 'text/html',
+                '.js': 'text/javascript',
+                '.css': 'text/css',
+                '.json': 'application/json',
+                '.jpg': 'image/jpg',
+                '.gif': 'image/gif' }; //Define acceptable file extensions
+  let type = mime[extension] || 'application/octet-stream'; //If required file type is not mime, default to an arbitrary binary data
+
+  fs.readFile(file, (error, content) => { //Read the file from the hard drive
+    if(error) {
+      if (error.code == 'ENOENT') {
+        fs.readFile('./404.html', (error, content) => {
+          response.writeHead(200, {'Content-Type': type});
+          response.end(content, 'utf-8');
+        });
+      } else {
+        response.writeHead(500);
+        response.end('Error: ' + error.code + '\n');
+        response.end();
+      }
+    } else {
+      response.writeHead(200, {'Content-Type': type});
+      response.end(content, 'utf-8');
+    }
+  });
+
+}).listen(port, ip);
+
+console.log('Running at ' + ip + ':' + port + '/');
